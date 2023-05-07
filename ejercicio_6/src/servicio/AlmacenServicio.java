ab696e457719a10254b59784dc110f1622e0ce22
Aaron Vigil <aaronvigil96@gmail.com>
2023-05-07 17:22:12 -0300

    ejercicio 6

---
+++ ejercicio_6/src/servicio/AlmacenServicio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicio;
+
+import entidad.Almacen;
+
+public class AlmacenServicio {
+    
+   public Almacen crearAlmacen(){
+       Almacen almacen = new Almacen();
+       return almacen;
+   }
+   public void ingresarProducto(){
+        
+   }
+}
